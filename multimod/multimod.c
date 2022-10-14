fc9de40a05b6e559388cc85dbec5275275e474d4
211300016-Sun Zhen <211300016@smail.nju.edu.cn>
2022-10-14 15:42:30 +0800

    advance mod

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,11 +1,34 @@
 #include <stdint.h>
+#include <stdio.h>
 
+int getBit(uint64_t a) {
+	int i=0;
+	for (i=1; a > 0; i++)
+		a >>= 1;
+	return i;
+}
+
+uint64_t Mod(uint64_t a, uint64_t m) {
+	if (a < m) return a;
+	int bitA=getBit(a);
+	int bitB=getBit(m);
+	uint64_t M = m << (bitA-bitB);
+	while (M >= m) {
+		if (a >= M) {
+			a = a - M;
+		}
+		M = M >> 1;
+	}
+	return a;
+}
+
+/*
 uint64_t Mod(uint64_t a, uint64_t m) {
 	while (a >= m) {
 		a -= m;
 	}
 	return a;
-}
+}*/
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
 	uint64_t sum = 0;
