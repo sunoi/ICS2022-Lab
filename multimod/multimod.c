ea57d1c4fc500f1403e63566c7ab19f087f3539e
211300016-Sun Zhen <211300016@smail.nju.edu.cn>
2022-10-14 14:05:12 +0800

    nothing

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,14 +1,15 @@
 #include <stdint.h>
+#include <stdio.h>
+#include <inttypes.h>
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
 	uint64_t sum = 0;
-	//tag
 	while (a >= 1) {
 		if (a&1 == 1) {
-			sum = (sum+b)&(m-1);
+			sum = ((sum+b)&(m-1));
 		}
 		a = a >> 1;
-		b = (b << 1)&(m-1);
+		b = ((b << 1)&(m-1));
 	}
   return sum;
 }
