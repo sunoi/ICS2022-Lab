ffdcc5bd6a39c24550a9749102a837b2493cb4f0
211300016-Sun Zhen <211300016@smail.nju.edu.cn>
2022-10-15 11:51:24 +0800

    solve the overflow

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -22,22 +22,25 @@ uint64_t Mod(uint64_t a, uint64_t m) {
 	return a;
 }
 
-/*
-uint64_t Mod(uint64_t a, uint64_t m) {
-	while (a >= m) {
-		a -= m;
-	}
-	return a;
-}*/
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
 	uint64_t sum = 0;
+	//printf("m=%lu\n", ~m);
 	while (a >= 1) {
 		if ((a&1) == 1) {
-			sum = Mod(sum + b, m);
+			uint64_t temp = sum + b;
+			if (temp < sum || temp < b) {
+				//printf("%lu  ", temp);
+				temp = Mod(Mod(temp, m) + (~m)+1, m);
+				//printf("temp=%lu\n", temp);
+			}
+			sum = Mod(temp, m);
 		}
 		a = a >> 1;
-		b = Mod((b << 1), m);
+		if (b << 1 < b) {
+			b = Mod((b << 1) + (~m) + 1, m);
+		}
+		else b = Mod((b << 1), m);
 	}
   return sum;
 }
